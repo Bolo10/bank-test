f443fefec2a9c419199cc8720bf5b4ef39ea03f1
erick.tapia <erick.tapia850@gmail.com>
2026-01-13 21:52:47 -0500

    set up the structure, layout created, running locally succesfull

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ShellComponent } from './layout/shell/shell.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [{ path: '', pathMatch: 'full', redirectTo: 'clientes' }],
+  },
+  { path: '**', redirectTo: '' },
+];
